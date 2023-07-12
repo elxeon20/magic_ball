496a7062b432777793ac197b526352110f9f1d10
elxeon20 <elxeon20@gmail.com>
2023-07-12 20:33:46 +0300

    finished project

---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() => runApp(
+      MaterialApp(
+        home: BallPage(),
+      ),
+    );
+
+class BallPage extends StatelessWidget {
+  const BallPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: Text("Ask Me Anything"),
+        backgroundColor: Colors.indigo,
+      ),
+      body: ball(),
+    );
+  }
+}
+
+class ball extends StatefulWidget {
+  const ball({super.key});
+
+  @override
+  State<ball> createState() => _ballState();
+}
+
+class _ballState extends State<ball> {
+  int ballNumber = 3;
+
+  void randomNumber() {
+    ballNumber = Random().nextInt(4) + 1;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        setState(() {
+          randomNumber();
+        });
+      },
+      child: Center(
+        child: Image.asset("images/ball$ballNumber.png"),
+      ),
+    );
+  }
+}
